7ed6dcfca42fcbfcdf7493afb7ce3210f3db8cab
pereiraldm <lucaspereira@iPro-de-Lucas.local>
2024-04-29 12:40:29 -0700

    Adding brightness

--- Lunex Smart Light/AdvancedView.swift
+++ Lunex Smart Light/AdvancedView.swift
@@ -11,6 +11,8 @@ struct AdvancedView: View {
     @EnvironmentObject var bluetoothViewModel: BluetoothViewModel
     @State private var isToggleButtonEnabled: Bool = false
     @State private var sliderValue: Double = 0
+    @State private var brightnessValue: Double = 0
+    @State private var lastBrightnessValue: Double? = nil
     @State private var lastSliderValue: Double? = nil
     
     var adjustedSliderValue: Double {
@@ -24,7 +26,13 @@ struct AdvancedView: View {
                 .edgesIgnoringSafeArea([.leading, .trailing])
         VStack {
             if bluetoothViewModel.isConnected {
-                
+                HStack {
+                    Image(systemName: bluetoothViewModel.whiteColdLightOn ? "lightbulb.fill" : "lightbulb")
+                        .foregroundColor(bluetoothViewModel.whiteColdLightOn ? .white : .gray)
+                    
+                    Image(systemName: bluetoothViewModel.whiteHotLightOn ? "lightbulb.fill" : "lightbulb")
+                        .foregroundColor(bluetoothViewModel.whiteHotLightOn ? .yellow : .gray)
+                }
                 Rectangle()
                     .fill(Color(hex: "#f37021"))
                     .frame(height: 40)
@@ -38,14 +46,31 @@ struct AdvancedView: View {
                 
                     .padding()
                 
-                HStack {
-                    Image(systemName: bluetoothViewModel.whiteColdLightOn ? "lightbulb.fill" : "lightbulb")
-                        .foregroundColor(bluetoothViewModel.whiteColdLightOn ? .white : .gray)
-                    
-                    Image(systemName: bluetoothViewModel.whiteHotLightOn ? "lightbulb.fill" : "lightbulb")
-                        .foregroundColor(bluetoothViewModel.whiteHotLightOn ? .yellow : .gray)
-                }
                 
+                HStack {
+                    Text("ATIVAR")
+                        .foregroundColor(Color(hex: "#1b2c5d"))
+                        .bold()
+                    
+                    Toggle("", isOn: $isToggleButtonEnabled)
+                        .labelsHidden()
+                }
+                .padding() // Aplica padding ao HStack para o espaçamento interno
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(hex: "#f37021"))
+                )
+                .onChange(of: isToggleButtonEnabled) { isEnabled in
+                    if isEnabled {
+                        bluetoothViewModel.sendCommand("avancado")
+                        // Enviar o valor atual do Slider imediatamente após ativar o ToggleButton
+                        bluetoothViewModel.sendCommand("\(sliderValue)")
+                    } else {
+                        bluetoothViewModel.sendCommand("desligarAvancado")
+                    }
+                }
+                //Spacer()
+                VStack(alignment: .leading) {
                 Slider(value: $sliderValue, in: 0...255, step: 1)
                     .disabled(!isToggleButtonEnabled)
                     .onChange(of: sliderValue) { newValue in
@@ -59,41 +84,40 @@ struct AdvancedView: View {
                         }
                     }
                 
-                HStack {
-                    HStack {
-                        Text("ATIVAR")
-                            .foregroundColor(Color(hex: "#1b2c5d"))
-                            .bold()
-                        
-                        Toggle("", isOn: $isToggleButtonEnabled)
-                            .labelsHidden()
-                    }
-                    .padding() // Aplica padding ao HStack para o espaçamento interno
-                    .background(
-                        RoundedRectangle(cornerRadius: 10)
-                            .fill(Color(hex: "#f37021"))
-                    )
-                    .onChange(of: isToggleButtonEnabled) { isEnabled in
-                        if isEnabled {
-                            bluetoothViewModel.sendCommand("avancado")
-                            // Enviar o valor atual do Slider imediatamente após ativar o ToggleButton
-                            let mappedValue = Int(sliderValue * (1024 / 255))
-                            bluetoothViewModel.sendCommand("\(mappedValue)")
-                        } else {
-                            bluetoothViewModel.sendCommand("desligarAvancado")
-                        }
-                    }
-                    Spacer()
-                    HStack{
-                        Text("VALOR: \(Int(adjustedSliderValue)) (3000 - 6500)") .foregroundColor(Color(hex: "#1b2c5d"))
-                            .bold()
-                    }
+                HStack{
+                    Text("COR: \(Int(adjustedSliderValue)) (3000 - 6500)") .foregroundColor(Color(hex: "#1b2c5d"))
+                        .bold()
+                }
                 .padding()
                 .background(
                     RoundedRectangle(cornerRadius: 10)
                         .fill(Color(hex: "#f37021"))
                 )
+            }
+                VStack(alignment: .leading) {
+                Slider(value: $brightnessValue, in: 0...100, step: 1)
+                    .disabled(!isToggleButtonEnabled)
+                    .onChange(of: brightnessValue) { newValue in
+                        lastBrightnessValue = newValue
+                        // Iniciar um timer para atrasar o envio do comando
+                        Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { _ in
+                            if lastBrightnessValue == newValue { // Verifica se o valor do Slider não mudou
+                                bluetoothViewModel.sendCommand("INTENSIDADE: \(String(describing: newValue))")
+                            }
+                        }
+                    }
+                
+                HStack {
+                    Text("INTENSIDADE: \(Int(brightnessValue)) (0 - 100)") .foregroundColor(Color(hex: "#1b2c5d"))
+                        .bold()
                 }
+                
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(hex: "#f37021"))
+                )
+            }
                
             } else {
                 Rectangle()
